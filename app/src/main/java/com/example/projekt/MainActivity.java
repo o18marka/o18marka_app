66e13d467ff8eb2ed7de6c5b659df3594d362e5a
o18marka <o18marka@student.his.se>
2019-05-13 10:57:34 +0200

    Listview visar nu data hämtat från nätet(Testas med JSON datan som gavs i tidigare JSON uppgiften)

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.projekt;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] allaobjekt = {"Testobjekt", "Testobjekt2"};
-    private String[] allinfo = {"Testobjekt3", "Testobjekt4"};
+    private String[] allaobjekt = {" "};
+    private String[] allinfo = {" "};
     private int[] allinfo2 ={};
-    private ArrayList<String> listData=new ArrayList<>(Arrays.asList(allaobjekt));
-    private ArrayList<Minklass> minAdapter=new ArrayList<>();
+    private ArrayList<String> listData;
+    private ArrayAdapter<Minklass> minAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +43,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ArrayAdapter<String>adapter=new ArrayAdapter<>(this,R.layout.content_main,R.id.my_item, listData);
+        new FetchData().execute();//Hämtar datan
+        listData=new ArrayList<>(Arrays.asList(allaobjekt)); //Listar den hämtade datan i mountainNames fältet
+        minAdapter=new ArrayAdapter<Minklass>(this,R.layout.content_main,R.id.my_item);
+
         ListView my_listview=(ListView) findViewById(R.id.my_listview); //Skapar en listview
-        my_listview.setAdapter(adapter);
+        my_listview.setAdapter(minAdapter); //Listview ska innehålla data från mountainAdapter
 
-        minAdapter.add(new Minklass("Matterhorn","Alps",4478)); //här
-        minAdapter.add(new Minklass("Mont Blanc","Alps",4808)); //här
-        minAdapter.add(new Minklass("Denali","Alaska",6190)); //här
-
-        ArrayAdapter<Minklass> adapter2=new ArrayAdapter<>(this,R.layout.content_main,R.id.my_item,minAdapter);
-
-        listData=new ArrayList<>(Arrays.asList(allaobjekt)); //Listar datan i allaobjekt fältet
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() { //Toast som bestämmer vilken information som ska printas beroende på variabeln i (senare skapad for-loop)
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String temp = minAdapter.getItem(i).info();
+                Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,4 +84,94 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class FetchData extends AsyncTask<Void,Void,String> {
+        @Override
+
+        protected String doInBackground(Void... params) {
+            // These two variables need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a Java string.
+            String jsonStr = null;
+
+            try {
+                // Construct the URL for the Internet service
+                URL url = new URL("http://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom"); //URL
+
+                // Create the request to the PHP-service, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in
+                // attempting to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Network error", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            Log.d("brom","DataFetched:"+o);
+
+            try {
+                JSONArray toastArray = new JSONArray(o);
+                for (int i = 0; i < toastArray.length(); i++) { //for-loop som hämtar alla berg i php dokumentet som sedan läggs till i strängen m som lägggs till i adaptern som listas i listview
+                    Log.d("brom", "element 0:" + toastArray.get(i).toString());
+                    JSONObject container = toastArray.getJSONObject(i);
+                    //loggar data om bergen i objectet container
+                    Log.d("brom", container.getString("name"));
+                    Log.d("brom", container.getString("location"));
+                    Log.d("brom", "" + container.getInt("size"));
+                    Minklass m = new Minklass(container.getString("name"), container.getString("location"), container.getInt("size"));
+                    Log.d("brom", m.toString());
+                    minAdapter.add(m);
+                }
+            }
+            catch (JSONException e){
+                Log.e("brom","E:"+e.getMessage());
+            }
+        }
+    }
 }
+
+
+
