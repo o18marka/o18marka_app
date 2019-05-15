703eb8e07e9c91cd05d9ad797fc877afaafbdb23
o18marka <o18marka@student.his.se>
2019-05-15 11:54:04 +0200

    Refresh-knapp tillagd istället för föregående settings-knapp utan funktion

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -82,7 +82,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_refresh){
+            minAdapter.clear(); //Rensar listan vid refresh så gamla versioner rensas
+            new FetchData().execute();
             return true;
         }
 
